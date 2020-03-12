5377bb13c724cca7e20c64af05109902ff31bd0b
Lukas Betancur <betancurlukas98@gmail.com>
2020-03-12 13:34:43 -0400

    commit 28

--- src/Definitions.java
+++ src/Definitions.java
@@ -2,6 +2,10 @@ public class Definitions{
     private String definition;
 	private String partOfSpeech;
 	
-
+	
+	public Definitions(String definition, String partsOfSpeech) {
+		this.definition = definition;
+		this.partOfSpeech = partsOfSpeech;
+	}
 
 }
