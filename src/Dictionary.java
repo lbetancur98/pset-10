117185f3976f4bc4cffcc56c6e6dafb6b4cfb67c
Lukas Betancur <betancurlukas98@gmail.com>
2020-03-12 12:41:18 -0400

    commit 13

---
+++ src/Dictionary.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException; 
+
+public class Dictionary {
+
+    
+}
