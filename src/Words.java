f9c8f7cf39f0a260fb9868e88637361f5437b6a9
Lukas Betancur <betancurlukas98@gmail.com>
2020-03-12 13:28:29 -0400

    commit 21

--- src/Words.java
+++ src/Words.java
@@ -3,7 +3,16 @@ import java.util.ArrayList;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException; 
 public class Words {
-
+    ArrayList<Definitions> definitions = new ArrayList<Definitions>(); 
+	ArrayList<String> synonyms = new ArrayList<String>();
+	ArrayList<String> antonyms = new ArrayList<String>();
+	
+	public Words(String word, ArrayList<Definitions> definitions, ArrayList<String> synonyms, ArrayList<String> antonyms) throws JsonSyntaxException, JsonIOException, FileNotFoundException {
+		this.word = word.substring(0, 1).toUpperCase() + word.substring(1, word.length()).toLowerCase();
+		this.definitions = definitions;
+		this.synonyms = synonyms;
+		this.antonyms = antonyms;
+	}
 
 
 }
